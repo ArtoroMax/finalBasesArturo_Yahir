b4d1ed8560e91e26c9b13071d39a1cbf692448ea
EDWIN GARCÉS <arteconedwin2803@gmail.com>
2020-03-19 19:14:15 -0500

    formularios 1 2 3, enlace a datos de 1 y 2.

--- ProyectoFinan_HG/ProyectoFinan_HG/Form1.cs
+++ ProyectoFinan_HG/ProyectoFinan_HG/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ProyectoFinan_HG.logica;
 
 namespace ProyectoFinan_HG
 {
@@ -15,6 +16,37 @@ namespace ProyectoFinan_HG
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        EmpresaDomiciliaria empresa = new EmpresaDomiciliaria();
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            int nit, ccomnit, resultado;
+            string nombre, fecha;
+            nit = int.Parse(txtNit.Text);
+            ccomnit = int.Parse(txtNitcamara.Text);
+                
+            nombre = txtNombre.Text;
+            fecha = string.Format("yyyy-mm-dd",dtpFecha);
+
+            /*paso 2
+            *enviar las variables a la capa de la lógica*/
+
+            resultado = empresa.ingresarEmpresa(nit, nombre, fecha, ccomnit);
+            if (resultado > 0)
+            {
+                MessageBox.Show("Empresa Domiciliaria Registrado", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Empresa Domiciliaria  no Registrado", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
---
+++ ProyectoFinan_HG/ProyectoFinan_HG/logica/EmpresaDomiciliaria.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Oracle.DataAccess.Client;
+
+
+namespace ProyectoFinan_HG.logica
+{
+    class EmpresaDomiciliaria
+    {
+        /*Paso 0: creo un objeto para comunicarme con la capa de accesoDatos y la clase Datos*/
+        Datos dt = new Datos();
+        public int ingresarEmpresa(int nit, string nombre, string fecha, int nitCcom)
+        {
+            int result = 0;
+            string consulta;
+            /* se arma la consulta*/
+            consulta = "insert into EmpresaDomiciliaria(edomNit, edomNombre, edomFechaOp, ccomNit) values(" + nit + ",'" + nombre + "','" + fecha + "','" + nitCcom + "')";
+            /* se envía la consulta a la capa de accesoDatos PARA SER EJECUTADA*/
+            result = dt.ejecutarDML(consulta);
+            return result;
+        }
+
+        public DataSet consultarEmpresa()
+        {
+            string consulta;
+            DataSet miDS = new DataSet();
+            /* se arma la consulta*/
+            consulta = "select * from EmpresaDomiciliaria";
+            /* se envía la consulta a la capa de accesoDatos PARA SER EJECUTADA*/
+            miDS = dt.ejecutarSELECT(consulta);
+            return miDS;
+        }
+    }
+}
